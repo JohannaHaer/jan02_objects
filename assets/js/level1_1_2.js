38c400f5c9cda8a8fae75f5a2b687d2d49eb5e63
JohannaHaer <johanna.haerdter@gmail.com>
2024-01-02 12:00:04 +0100

    level1 1.2

---
+++ assets/js/level1_1_2.js
@@ -0,0 +1,10 @@
+const person = {
+    name: "Johanna",
+    alter: 30,
+    sagNameAlter: () => {
+        alert(`Mein Name ist ${person.name} und ich bin ${person.alter} Jahre alt`)
+    }
+}
+
+person.sagNameAlter()
+
