f4126caf43778f2329c7f926e13956999b28e1db
JohannaHaer <johanna.haerdter@gmail.com>
2024-01-03 17:16:59 +0100

    level 2.3

--- assets/js/level1_2_3.js
+++ assets/js/level1_2_3.js
@@ -10,29 +10,52 @@ const singers = [
 ];
 
 
-console.log(singers);
+let mapName = singers.map((singerName) => singerName.name)
+console.log(mapName);
 
-// let singersName = singers[0]
-// console.log(singersName);
+let mapCountry = singers.map((singerCountry) => singerCountry.country)
+console.log(mapCountry);
 
+let mapStart = singers.map((singerStart) => singerStart.period_active.start)
+console.log(mapStart);
 
+let mapEnd = singers.map((singerEnd) => singerEnd.period_active.end)
+console.log(mapEnd);
 
-// const  singerNames = singers.map((singer) => singer)
-
-// console.log(singerNames);
-
-
-// const sortBandsAlphabetically = singerNames.map((singer) => singer.name).sort()
-
-// console.log(sortBandsAlphabetically);
-
+let mapGenre = singers.map((singerGenre) => singerGenre.genre)
+console.log(mapGenre);
 
 
-const singersAlphabetically = singers.sort((a, b) => {
-    if(a.name < b.name){
-        return -1;
-    }else if(a.name > b.name){
-        return 1;
-    }
-})
-console.log(singers);
+let table = document.createElement('table');
+let thead = document.createElement('thead');
+let tbody = document.createElement('tbody');
+
+document.querySelector('body').appendChild(table);
+
+table.appendChild(thead);
+table.appendChild(tbody);
+
+let row_1 = document.createElement('tr')
+let heading_1 = document.createElement('th')
+heading_1.innerHTML = 'Name'
+let heading_2 = document.createElement('th')
+heading_2.innerHTML = 'Country'
+let heading_3 = document.createElement('th')
+heading_3.innerHTML = 'Period_active'
+let heading_4 = document.createElement('th')
+heading_4.innerHTML = 'Genre'
+
+
+
+row_1.appendChild(heading_1)
+row_1.appendChild(heading_2)
+row_1.appendChild(heading_3)
+row_1.appendChild(heading_4)
+tbody.appendChild(row_1)
+
+
+for (let i = 0; i < mapName.length; i++){
+    let rowStart = document.createElement('tr')
+    rowStart.innerHTML = `<th>${mapName[i]}</th> <th>${mapCountry[i]}</th> <th>${mapStart[i]} - ${mapEnd[i]}</th> <th>${mapGenre[i]}</th>`
+    tbody.appendChild(rowStart)
+}
